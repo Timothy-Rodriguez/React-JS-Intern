0763e3a61ad9ad927dcba8d7b4b7dbbb3305da87
unknown <mtimothyrodriguez2000@gmail.com>
2020-11-30 22:17:11 +0530

    Day4 added

---
+++ Day4/2_multiples.js
@@ -0,0 +1,16 @@
+//JS program to find correct multiples.
+
+for(i=1;i<=100;i++){
+    if(i%3 == 0 && i%5 == 0){
+        console.log('bestenlistreact');
+    }
+    else if(i%3 == 0){
+        console.log('bestenlist');
+    }
+    else if(i%5 == 0){
+        console.log('react');
+    }
+    else{
+        console.log(i);
+    }
+}
